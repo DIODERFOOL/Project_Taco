533d3fb36be3bccb1a6e63fb26d4c0e166e9fd60
Alejandro Lopez Martinez <alejandrolm14@gmail.com>
2019-02-22 16:17:08 -0600

    Agregado, cambio de escena

---
+++ Proyecto Taco/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private string loadLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+        
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Entro en el camvio de escena");
+            SceneManager.LoadScene(loadLevel);
+        }
+    }
+}
