2aa4a250b9e3c4d8083bf4bc2604d0da58f46ad8
Rafael Rojas <yugnarcorp@hotmail.com>
2019-04-05 13:27:17 -0600

    Room B almost complete

---
+++ Proyecto Taco/Assets/Scripts/HoverV2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverV2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = gameObject.transform.position;
+        position.y =  position.y + 0.005f*Mathf.Cos(Time.time);
+        gameObject.transform.position = position;
+    }
+}
