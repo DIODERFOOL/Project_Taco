8be30e91298e45ac4dfd8a4ff4b27cc6561b0658
Rafael Rojas <yugnarcorp@hotmail.com>
2019-05-17 15:29:53 -0500

    Semi Final Changes

---
+++ Proyecto Taco/Assets/Scripts/SpeechRoomFinal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechRoomFinal : MonoBehaviour
+{
+    public GameObject playerSprite;
+    public GameObject machoTextCanvas;
+
+    private bool showMachoSpeech;
+
+    public Text dynamicSpeech; 
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var position = playerSprite.transform.position;
+        if (position.x >= 58.5 && position.x <= 61)
+        {
+            showMachoSpeech = true;
+            machoTextCanvas.SetActive(showMachoSpeech);
+            dynamicSpeech.text = "Has obtenido el primer tesoro del cenote de los dioses. Sigue buscando tesoros en la versión completa de <b>SANCHEZ: DIE TWICE COMING STEAM SUMMER 2020.</b>";
+            GManagerC.instance.clearRoomC();
+        }
+        else
+        {
+            showMachoSpeech = false;
+            machoTextCanvas.SetActive(showMachoSpeech);
+        }
+    }
+}
